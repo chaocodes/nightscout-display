acf4c7c7918450938b478e1a6e32b20abbc9607e
Chao Lai <chaol@qualtrics.com>
2025-09-14 14:48:45 -0700

    Test failure

--- test/utils/test_threshold.py
+++ test/utils/test_threshold.py
@@ -3,7 +3,7 @@ from src.utils.status import Status
 
 
 def test_threshold() -> None:
-    assert Threshold.get_status(100) == Status.OK
+    assert Threshold.get_status(100) == Status.HIGH
     assert Threshold.get_status(190) == Status.HIGH
     assert Threshold.get_status(299) == Status.HIGH_CRITICAL
     assert Threshold.get_status(70) == Status.LOW
